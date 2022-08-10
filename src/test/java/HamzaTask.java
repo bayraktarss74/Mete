8c35461528f10ec15cf95edb958174f446fcf0eb
HamzaE <hamzaeraslan6@gmail.com>
2022-08-10 22:09:40 +0300

    HamzaTask.java is added

--- src/test/java/HamzaTask.java
+++ src/test/java/HamzaTask.java
@@ -1,7 +1,7 @@
 public class HamzaTask {
     public static void main(String[] args) {
 
-        System.out.println("Hi again to everyone");
+        System.out.println("Hi again to Everyone");
 
 
 
